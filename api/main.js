f48bc509aae16ce4ced514152920d2ebbce36607
giafreisanctun-netizen <giafreisanctun@gmail.com>
2026-06-10 11:40:22 -0300

    Create main.js

---
+++ api/main.js
@@ -0,0 +1,25 @@
+import { createServer } from 'http';
+import express from 'express';
+import session from 'express-session';
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import { registerLoginRoutes } from "../server/_core/loginRoutes.js";
+import { getSessionConfig } from "../server/_core/session.js";
+import { appRouter } from "../server/routers.js";
+import { createContext } from "../server/_core/context.js";
+
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(session(getSessionConfig()));
+
+registerLoginRoutes(app);
+
+app.use(
+  "/api/trpc",
+  createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
+
+export default app;
